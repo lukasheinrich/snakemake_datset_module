5380d98595d7c67b364dbe4911aa7c2dc8125a41
Lukas Heinrich <lukas.heinrich@cern.ch>
2026-03-13 18:07:59 +0100

    first commit

--- what.sh
+++ what.sh
@@ -7,20 +7,22 @@
 
 tar -xzvf signal.tar.gz
 
+# 2 prep output dir
 mkdir signal_scaled
 
 
+# 3 map files while considering meta data
 python scripts/process.py signal/file1.txt signal/meta.json signal_scaled/file1.txt
 python scripts/process.py signal/file2.txt signal/meta.json signal_scaled/file2.txt
 python scripts/process.py signal/file3.txt signal/meta.json signal_scaled/file3.txt
 
 
-#create new metadata
+# 4 create new metadata
 
 cat signal_scaled/file*.txt|awk '{a+=$1}END{print a}'|jq '.|{sum: .}' > signal_scaled/meta.json
 
 
 
-# repack dataset
+# 5 repack dataset
 
 tar -czvf signal_scaled.tar.gz signal_scaled/*
